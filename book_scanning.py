8b36f2ab696e4f95539b377bad15194c506b6537
Devansh Sehgal <devanshsehgal02@gmail.com>
2021-02-06 09:47:56 +0530

    added empty main function

--- book_scanning.py
+++ book_scanning.py
@@ -1,3 +1,10 @@
 #!python3
 
-# Python Code for solving Book Scanning 2020 question 
+# Python Code for solving Book Scanning 2020 question
+
+def main():
+    
+
+
+if __name__ == ‘_main__’:  # single underscore
+    main()
